4d6d9dd619cd73b164c5136b0deab6f34a9fad49
Marcus Lunzenauer <mlunzena@uos.de>
2012-11-13 14:28:48 +0100

    adapt votes/index

--- app/views/votes/index.php
+++ app/views/votes/index.php
@@ -1,51 +1,67 @@
 <table border="0" cellpadding="2" cellspacing="0" width="99%">
     <tr>
        <th class="steel">&nbsp;</th>
-       <th class="steel" align="left">Titel</th>
-       <th class="steel">Fragen</th>
+       <th class="steel" align="left"><?= _("Titel") ?></th>
+       <th class="steel"><?= _("Antwortmöglichkeiten") ?></th>
        <th class="steel">&nbsp;</th>
        <th class="steel">&nbsp;</th>
        <th class="steel">&nbsp;</th>
     </tr>
-    
-    <?php
-    $i = 1;
-    foreach($this->votes as &$vote):
-        $showUrl = PluginEngine::GetLink($GLOBALS["plugin"], array(),
-                'cliqrplugin/show/' . $vote->getVoteID());
-        $resultsUrl = PluginEngine::GetLink($GLOBALS["plugin"], array(),
-                'cliqrplugin/results/' . $vote->getVoteID());
-        $startUrl = PluginEngine::GetLink($GLOBALS["plugin"], array(),
-                'cliqrplugin/start/' . $vote->getVoteID());
-        $stopUrl = PluginEngine::GetLink($GLOBALS["plugin"], array(),
-                'cliqrplugin/stop/' . $vote->getVoteID());
+
+<?
+$i = 1;
+foreach ($this->votes as $vote) {
+    $showUrl    = $controller->url_for('votes/show',    $vote->getVoteID());
+    $resultsUrl = $controller->url_for('votes/results', $vote->getVoteID());
+    $startUrl   = $controller->url_for('votes/start',   $vote->getVoteID());
+    $stopUrl    = $controller->url_for('votes/stop',    $vote->getVoteID());
     ?>
     <tr>
-        <td class="steel1"><?php echo "$i." ?></td>
-        <td class="steel1" align="left"><?php echo $vote->getTitle() ?></td>
+        <td class="steel1"><?= $i++ ?>.</td>
+        <td class="steel1" align="left"><?= htmlReady($vote->getTitle()) ?></td>
         <td class="steel1" align="center">
-            <?php echo count($vote->getAnswers()) ?>
+            <?= count($vote->getAnswers()) ?>
         </td>
         <td class="steel1" align="center">
-            <?php
-            printf("<a href='%s'>anzeigen</a>", $showUrl);
-            ?>
+            <a href="<?= $showUrl ?>"><?= ("anzeigen") ?></a>
         </td>
         <td class="steel1" align="center">
-            <?php
-            if(!$vote->isActive()):
-                printf("<a href='%s'>Start</a>", $startUrl);
-            else:
-                printf("<a href='%s'>Stop</a>", $stopUrl);
-            endif;
-            ?>
+              <? if ($vote->isActive()) { ?>
+                  <a href="<?= $stopUrl ?>"><?= _("Start") ?></a>
+              <? } else { ?>
+                  <a href="<?= $startUrl ?>"><?= _("Stop") ?></a>
+              <? } ?>
         </td>
         <td class="steel1" align="center">
-            <a href="<?php echo $resultsUrl; ?>">Ergebnisse</a>
+            <a href="<?= $resultsUrl ?>"><?= _("Ergebnisse") ?></a>
         </td>
     </tr>
-    <?php
-        $i++;
-    endforeach;
-    ?>
+<? } ?>
 </table>
+
+<ol id="questions">
+  <? foreach ($this->votes as $vote) { ?>
+
+  <? $alt = sprintf('alt="%s" title="%1$s"', htmlReady($vote->getTitle())); ?>
+
+  <li <?= $alt ?>>
+    <div class="question">
+
+      <a class="hover" href="<?= $controller->url_for('votes/show', $vote->getVoteID()) ?>">
+        <span class="show_audience" <?= $alt ?>>
+          Show audience
+        </span>
+      </a>
+
+      <div class="control">
+        <a class="edit"   alt="edit"   title="edit"   href="<?= $controller->url_for('test') ?>"></a>
+        <a class="delete" alt="delete" title="delete" href="/d/lkk"></a>
+      </div>
+    </div>
+
+    <div class="title" <?= $alt ?>>
+      <?= htmlReady($vote->getTitle()) ?>
+    </div>
+  </li>
+  <? } ?>
+</ol>
