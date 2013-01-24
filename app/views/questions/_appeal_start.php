721c2b45ded0ec0c92e225c9d11b6f596de60949
Marcus Lunzenauer <mlunzena@uos.de>
2013-01-24 10:38:49 +0100

    visibly offer restart of stopped questions

--- app/views/questions/_appeal_start.php
+++ app/views/questions/_appeal_start.php
@@ -1,6 +1,6 @@
 <form action="<?= $controller->url_for('questions/start', $id) ?>" method="POST">
   <?= CSRFProtection::tokenTag() ?>
-  <?= \STUDIP\Button::create(_("Frage starten"), '') ?>
+<?= \STUDIP\Button::create($question->isNew() ? _("Frage starten") : _("Frage erneut starten"), '') ?>
 </form>
 <span class="loader">
   loading&hellip;
