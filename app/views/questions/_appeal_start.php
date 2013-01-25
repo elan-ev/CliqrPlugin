a31283ad419ceb90c98f213fcdc16dd3a97546d9
luniki <luniki@gmail.com>
2013-01-25 15:38:19 +0100

    remove unnecessary partial

--- app/views/questions/_appeal_start.php
+++
@@ -1,7 +0,0 @@
-<form action="<?= $controller->url_for('questions/start', $id) ?>" method="POST">
-  <?= CSRFProtection::tokenTag() ?>
-<?= \STUDIP\Button::create($question->isNew() ? _("Frage starten") : _("Frage erneut starten"), '') ?>
-</form>
-<span class="loader">
-  loading&hellip;
-</span>
--- app/views/questions/show.php
+++ app/views/questions/show.php
@@ -8,26 +8,29 @@ $state_text =
       ? _("läuft")
       : _("alt"));
 
+$poll_url = $controller->poll_url($cid);
+$short_url = $shortener->shorten($poll_url);
 ?>
 
 
 <div class="question state-<?= htmlReady($question->getState()) ?><?= $show_results ? '' : ' hide-results' ?>">
 
-
   <h2><span class="state"><?= $state_text ?></span> <?= htmlReady($question->getQuestion()) ?></h2>
 
   <?= $this->render_partial('questions/_show_controls', compact('id')) ?>
 
-  <?
-  $poll_url = $controller->poll_url($cid);
-  $short_url = $shortener->shorten($poll_url);
-  ?>
   <div class="appeal vote">
     <a class="short" alt="Vote at" href="<?= $poll_url ?>"><?= $short_url ?></a>
     <a class="qr" href="<?= $controller->url_for('qr', $cid) ?>"></a>
   </div>
   <div class="appeal start">
-    <?= $this->render_partial('questions/_appeal_start', compact('id')) ?>
+    <form action="<?= $controller->url_for('questions/start', $id) ?>" method="POST">
+      <?= CSRFProtection::tokenTag() ?>
+    <?= \STUDIP\Button::create($question->isNew() ? _("Frage starten") : _("Frage erneut starten"), '') ?>
+    </form>
+    <span class="loader">
+      loading&hellip;
+    </span>
   </div>
 
 </div>
