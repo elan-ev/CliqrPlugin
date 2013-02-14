40aa6ec394c7877862e0669d0fa3bd7877656165
luniki <luniki@gmail.com>
2013-02-14 08:47:52 +0100

    add #new route to QuestionsRouter

--- assets/js/routers/questions.coffee
+++ assets/js/routers/questions.coffee
@@ -3,13 +3,18 @@ define [
   'utils'
   'models/questions'
   'views/questions_index'
-], (Backbone, utils, QuestionCollection, QuestionsIndexView) ->
+  'views/questions_form'
+], (Backbone, utils, QuestionCollection, QuestionsIndexView, QuestionsForm) ->
 
   class QuestionsRouter extends Backbone.Router
 
     routes:
       "": "index"
+      "new": "newQuestion"
 
     index: ->
       questions = new QuestionCollection cliqr.config.POLLS
       utils.changeToPage new QuestionsIndexView collection: questions
+
+    newQuestion: ->
+      utils.changeToPage new QuestionsForm
--- assets/js/routers/questions.js
+++ assets/js/routers/questions.js
@@ -2,7 +2,7 @@
   var __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  define(['backbone', 'utils', 'models/questions', 'views/questions_index'], function(Backbone, utils, QuestionCollection, QuestionsIndexView) {
+  define(['backbone', 'utils', 'models/questions', 'views/questions_index', 'views/questions_form'], function(Backbone, utils, QuestionCollection, QuestionsIndexView, QuestionsForm) {
     var QuestionsRouter;
     return QuestionsRouter = (function(_super) {
 
@@ -13,7 +13,8 @@
       }
 
       QuestionsRouter.prototype.routes = {
-        "": "index"
+        "": "index",
+        "new": "newQuestion"
       };
 
       QuestionsRouter.prototype.index = function() {
@@ -22,6 +23,10 @@
         return utils.changeToPage(new QuestionsIndexView({
           collection: questions
         }));
+      };
+
+      QuestionsRouter.prototype.newQuestion = function() {
+        return utils.changeToPage(new QuestionsForm);
       };
 
       return QuestionsRouter;
