b9a98ecd4729be235873e6d860fa23fb6f667aa0
luniki <luniki@gmail.com>
2016-09-22 09:49:27 +0200

    set alt and title in icon helper

--- assets/js/helpers/icon.js
+++ assets/js/helpers/icon.js
@@ -1,10 +1,12 @@
 import { SafeString, escapeExpression } from 'handlebars/runtime'
 
 const icon = function (icon, { hash } ) {
-    let { color = 'blue' } = hash
+    let { color = 'blue', text = '' } = hash
+
+    text = escapeExpression(text)
 
     const icon_path = [STUDIP.ASSETS_URL, 'images/icons/', color, '/', escapeExpression(icon), '.svg'].join('')
-    return new SafeString(`<img class="cliqr--icon" src="${icon_path}">`)
+    return new SafeString(`<img class="cliqr--icon" src="${icon_path}" alt="${text}" title="${text}">`)
 }
 
 export default icon
