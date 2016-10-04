98d6c39d02c9d9fa4af90d19ccff8f0e48e106f9
luniki <luniki@gmail.com>
2016-10-04 07:54:43 +0200

    remove unused files

--- assets/hbs/_include_js_templates.php
+++
@@ -1,5 +0,0 @@
-<? foreach (glob($this->_factory->get_path() . "hbs/". ($prefix ?: '') ."*.hbs") as $template) { ?>
-<script id="cliqr-template-<?= htmlReady(current(explode(".", basename($template))))?>" type="text/html">
-<? include $template ?>
-</script>
-<? } ?>
