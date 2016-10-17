6f3039b7c4d4556857ccdbfa8280dd0e0dfdfbbc
luniki <luniki@gmail.com>
2016-10-17 12:16:48 +0200

    try timeago

--- assets/js/helpers/moment.js
+++ assets/js/helpers/moment.js
@@ -1,5 +1,14 @@
+import { SafeString } from 'handlebars/runtime'
+import timeago from 'timeago.js'
+import deLocale from 'timeago.js/locales/de'
+
+timeago.register('de_DE', deLocale);
+const ta = new timeago()
+
 const moment_helper = function (n) {
-    return (new Date(n)).toLocaleString()
+    const date = ta.format(new Date(n), 'de_DE')
+
+    return new SafeString(date)
 }
 
 export default moment_helper
