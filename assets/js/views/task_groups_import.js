0ac2cfd4fc5e009e3cf4a5e5d732765b6dbb11b1
luniki <luniki@gmail.com>
2016-10-20 15:04:49 +0200

    update import

--- assets/js/views/task_groups_import.js
+++ assets/js/views/task_groups_import.js
@@ -1,6 +1,13 @@
 import Backbone from 'backbone'
 import _ from 'underscore'
 
+import { showLoading } from '../utils'
+
+const cancel = function (e) {
+    e.stopPropagation()
+    e.preventDefault()
+}
+
 const TaskGroupsImportView = Backbone.View.extend({
 
     tagName: 'article',
@@ -8,7 +15,12 @@ const TaskGroupsImportView = Backbone.View.extend({
     className: 'cliqr--task-groups-import',
 
     events: {
-        'click .js-cancel': 'onClickCancel'
+        'click .js-cancel': 'onClickCancel',
+        'change input': 'onChangeFileInput',
+
+        'dragenter .cliqr--upload-box': cancel,
+        'dragover .cliqr--upload-box': cancel,
+        'drop .cliqr--upload-box': 'onDropFile'
     },
 
     initialize(options) {
@@ -29,6 +41,50 @@ const TaskGroupsImportView = Backbone.View.extend({
     onClickCancel(event) {
         event.preventDefault()
         this.trigger('cancel', event, this)
+    },
+
+    onChangeFileInput(event) {
+        event.preventDefault()
+        if (event.target.files.length) {
+            const fileReader = new FileReader()
+
+            fileReader.onload = (event) => {
+
+                try {
+                    JSON.parse(event.target.result)
+                } catch (err) {
+                    window.STUDIP.Dialog.confirm(
+                        'Die Datei hat nicht das richtige Format. Möchten Sie eine andere Datei hochladen?',
+                        () => {},
+                        () => this.trigger('cancel', event, this)
+                    )
+                    return
+                }
+
+                this.$('form').submit()
+                this.trigger('cancel', event, this)
+                showLoading()
+            }
+
+            fileReader.readAsText(event.target.files[0])
+        }
+    },
+
+    onDropFile(event) {
+        event.stopPropagation()
+        event.preventDefault()
+
+        const files = event.originalEvent.target.files || event.originalEvent.dataTransfer.files
+
+        if (files.length > 1) {
+            window.STUDIP.Dialog.confirm(
+                'Cliqr kann im Moment nur einzelne Dateien importieren. Möchten Sie es noch einmal probieren?',
+                () => {},
+                () => this.trigger('cancel', event, this)
+            )
+        } else {
+            this.$('input[type="file"]')[0].files = files
+        }
     }
 })
 
