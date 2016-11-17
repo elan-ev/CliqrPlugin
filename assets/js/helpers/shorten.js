161b9f0647f47fa49374476c496d8c0085629ae8
luniki <luniki@gmail.com>
2016-11-17 12:42:19 +0100

    defend helper "shorten" against bad input

--- assets/js/helpers/shorten.js
+++ assets/js/helpers/shorten.js
@@ -1,5 +1,5 @@
-function shorten (text, length) {
-    return text.length > length ? '' + text.slice(0, length - 2) + '[…]' : text
+function shorten(text, length) {
+    return !text || text.length <= length ? text : '' + text.slice(0, length - 2) + '[…]'
 }
 
 export default shorten
