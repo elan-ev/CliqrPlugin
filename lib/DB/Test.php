b08d7cc9b6f02cb8067153e3391b58f19373cf19
luniki <luniki@gmail.com>
2016-11-21 11:30:10 +0100

    remove unused code

--- lib/DB/Test.php
+++ lib/DB/Test.php
@@ -4,11 +4,6 @@ namespace Cliqr\DB;
 
 class Test extends \eAufgaben\DB\Test
 {
-    public function belongsToAVoting()
-    {
-        return isset($this->options['voting']) && $this->options['voting'] == 1;
-    }
-
     public function toJSON($omits = [])
     {
         $result = $this->toArray('id title created changed');
