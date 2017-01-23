f61a0ffa5f61ebfb934df3bd6c94923bc4fbcb91
luniki <luniki@gmail.com>
2017-01-23 09:58:08 +0100

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.html$/,
-                loader: 'file',
+                loader: 'file-loader',
                 query: {
                     name: '[name].[ext]'
                 }
