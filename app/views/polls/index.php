35e3fb1af5c8ff1a7287d5847cdf8be0f78fb444
Marcus <luniki@gmail.com>
2018-07-18 15:04:02 +0200

    remove unused css

--- app/views/polls/index.php
+++ app/views/polls/index.php
@@ -4,7 +4,6 @@
         <!-- Required meta tags always come first -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
         <title>Stud.IP-Cliqr: <?= htmlReady($course->name) ?></title>
         <?
         $PLGNURL = $plugin->getPluginURL();
