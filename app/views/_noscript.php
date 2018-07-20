c126bb8a499716aa85350c6257d7dcffec25feea
mlunzena <luniki@gmail.com>
2018-07-21 00:04:34 +0200

    use https for noscript

--- app/views/_noscript.php
+++ app/views/_noscript.php
@@ -2,7 +2,7 @@
       <h1>
       Um Cliqr verwenden zu können, aktivieren Sie in Ihrem Browser
       bitte JavaScript. Wenn Sie dabei Hilfe benötigen,
-      <a href="http://www.enable-javascript.com/de/"
+      <a href="https://www.enable-javascript.com/de/"
       target="_blank">können Sie diese Anleitung zu Rate ziehen.</a>
       </h1>
     </noscript>
