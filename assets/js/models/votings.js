5b248aa1d681591a0638558652eb665146e82baa
mlunzena <luniki@gmail.com>
2018-07-21 00:04:58 +0200

    split votings into polls

--- assets/js/models/votings.js
+++ assets/js/models/votings.js
@@ -1,32 +1,19 @@
 import Backbone from 'backbone'
 
 import Voting from './voting'
-import IDList from './id_list'
 
 const VotingsCollection = Backbone.Collection.extend({
 
     model: Voting,
 
     initialize() {
-        this.id_list = new IDList()
-        this.listenTo(this, 'add:response', this.onAddResponse)
     },
 
     url() {
         return window.cliqr.config.PLUGIN_URL + 'votings?cid=' + window.cliqr.config.CID
     },
 
-    comparator: 'start',
-
-    firstFresh() {
-        return this.find((model) => !this.id_list.test(model))
-    },
-
-    onAddResponse(model, response) {
-        if (!this.id_list.test(model)) {
-            this.id_list.add(model)
-        }
-    }
+    comparator: 'start'
 })
 
 export default VotingsCollection
