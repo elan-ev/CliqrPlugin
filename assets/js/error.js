8281d99d9b0b7e6d360dcc0a3639a0077f60716a
Marcus <luniki@gmail.com>
2018-11-27 14:22:36 +0100

    resize error dialog

--- assets/js/error.js
+++ assets/js/error.js
@@ -1,7 +1,7 @@
 export default function showError(errorMessage, data) {
     console.error(errorMessage, data)
     if (window.STUDIP && window.STUDIP.Dialog) {
-        window.STUDIP.Dialog.show(errorMessage)
+        window.STUDIP.Dialog.show(errorMessage, { title: 'Fehler', size: 'fit' })
     } else {
         alert(errorMessage)
     }
