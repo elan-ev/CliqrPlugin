e735563d8abcdeb64568dc1b699f9fe3fcb63176
Marcus <luniki@gmail.com>
2018-12-05 09:22:37 +0100

    delete votings from archive page

--- assets/js/views/archive.js
+++ assets/js/views/archive.js
@@ -1,13 +1,20 @@
+import Backbone from 'backbone'
 import _ from 'underscore'
 import Viewmaster from './viewmaster'
+import { showConfirmDialog } from '../dialog'
+import showError from '../error'
+import template from '../../hbs/archive.hbs'
 
 const ArchiveView = Viewmaster.extend({
-
     tagName: 'article',
 
     className: 'cliqr--archive',
 
-    template: require('../../hbs/archive.hbs'),
+    events: {
+        'click .js-remove': 'onClickRemove'
+    },
+
+    template,
 
     context() {
         return {
@@ -22,6 +29,31 @@ const ArchiveView = Viewmaster.extend({
                     }
                 })
         }
+    },
+
+    onClickRemove(event) {
+        event.preventDefault()
+
+        const voting = this.collection.get(
+            Backbone.$(event.target)
+                .closest('tr')
+                .data('votingid')
+        )
+        if (!voting) {
+            return
+        }
+
+        showConfirmDialog(`Wollen Sie diese Abstimmung wirklich löschen?`, () => {
+            voting
+                .destroy()
+                .then(() => {
+                    this.render()
+                    return null
+                })
+                .catch(error => {
+                    showError('Could not remove voting', error)
+                })
+        })
     }
 })
 
