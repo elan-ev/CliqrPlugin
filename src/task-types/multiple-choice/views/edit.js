ff56987a934d5333529fa596f906b369655fda86
mlunzena <luniki@gmail.com>
2019-07-17 07:18:43 +0200

    fix #134

--- src/task-types/multiple-choice/views/edit.js
+++ src/task-types/multiple-choice/views/edit.js
@@ -1,11 +1,15 @@
-import Backbone from 'backbone'
 import FormView from './form'
 
-const EditView = FormView.extend({
+export default FormView.extend({
     className: 'cliqr--multiple-choice-edit-view',
 
     onSubmitForm(event) {
         event.preventDefault()
+
+        this.model.set('task', {
+            ...this.model.get('task'),
+            answers: this.choices.toJSON()
+        })
         if (this.model.isValid()) {
             this.trigger('editTask', this.model)
         }
@@ -16,5 +20,3 @@ const EditView = FormView.extend({
         this.trigger('cancel', this.model)
     }
 })
-
-export default EditView
